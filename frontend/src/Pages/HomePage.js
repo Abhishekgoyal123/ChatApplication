9feb6894c235f66f4b56dc1945a48a4e9eb8035e
Abhishek <Abhishekgoyal8a@gmail.com>
2023-01-07 17:21:02 +0530

    .

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import {Box, Container, Text} from  "@chakra-ui/react";
 
 const HomePage = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Container >
+       <Box
+       display={"flex"}
+       alignItems={"center"}
+       flexDirection="row"
+       justifyItems="center"
+       justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius='1px'
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          vjh
+        </Text>
+
+      </Box >
+
+      <box>
+
+      </box>
+    </Container>
   )
-}
+};
 
 export default HomePage
